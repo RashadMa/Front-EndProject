606e60d8e1ba8bd109c8987b33380986b133d743
Rashad <87971037+RashadM0@users.noreply.github.com>
2021-12-16 21:50:47 +0400

    Update1.3

--- FrontEndProject/js/index.js
+++ FrontEndProject/js/index.js
@@ -1,28 +1,15 @@
-// const hamburgerIcon = document.querySelector(".hamburger-menu");
-// const navItems = document.querySelector(
-//   "#nav-list-ul"
-// );
-// const navLeft = document.querySelector(".nav-left-side");
-// hamburgerIcon.addEventListener("click", function () {
-//   navItems.classList.toggle("nav-hamburger-menu");
-// //   navItems.style.height = "200px"
-// //   navItems.classList.toggle("container");
-// //   navLeft.style.position = "relative"
-// //   console.log(navLeft);
-// });
-// function returnClick() {
-//     if (window.style) {
-
-//     }
-// }
-
+//navigation
 $(document).ready(function () {
   $(".hamburger-menu").on("click", function () {
     $("#nav-list-ul").slideToggle("fast");
     $("#nav-list-ul").css("background", "white")
   });
-  $(".submenu").on("click", function(){
-    // $("#first-submenu").toggleClass("active")
-    // $("#first-submenu").slideToggle("fast")
+  $(document).on("scroll", function(){
+    if (window.scrollY > 30) {
+      $(".navbar").addClass("sticky")
+    }else{
+      $(".navbar").removeClass("sticky")
+    }
   })
+
 })
