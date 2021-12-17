8cb53ac3131bad98afda948e66be4bc9427fd0cc
Rashad <87971037+RashadM0@users.noreply.github.com>
2021-12-17 20:30:31 +0400

    Update1.8

--- FrontEndProject/js/index.js
+++ FrontEndProject/js/index.js
@@ -2,27 +2,29 @@
 $(document).ready(function () {
   $(".hamburger-menu").on("click", function () {
     $("#nav-list-ul").slideToggle("fast");
-    $("#nav-list-ul").css("background", "white")
+    $("#nav-list-ul").css("background", "white");
   });
   //main
-  $(document).on("scroll", function(){
+  $(document).on("scroll", function () {
     if (window.scrollY > 30) {
-      $(".navbar").addClass("sticky")
-    }else{
-      $(".navbar").removeClass("sticky")
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
     }
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY > 140) {
-      $(".square-card").css("opacity", "1")
-      $(".square-card").css("transform", "translateY(0)")
+      $(".square-card").css("opacity", "1");
+      $(".square-card").css("transform", "translateY(0)");
     }
     if (window.scrollY > 700) {
-      $("#cloud-image").css("opacity", "1")
-      $("#cloud-image").css("padding-left", "85px")
+      $("#cloud-image").css("opacity", "1");
+      $("#cloud-image").css("transform", "translateX(0)");
     }
     if (window.scrollY > 1350) {
-      $("#cloud-image-part-2").css("opacity", "1")
-      $("#cloud-image-part-2").css("padding-left", "60px")
+      $("#cloud-image-part-2").css("opacity", "1");
+      $("#cloud-image-part-2").css("transform", "translateX(0)");
     }
-  })
-})
+  });
+  //counter
+  $(".num").counterUp({ delay: 10, time: 1000 });
+});
