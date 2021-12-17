32eadeb49487b7868c0dfe46db5ad4d48e1e4c96
Rashad <87971037+RashadM0@users.noreply.github.com>
2021-12-17 04:29:14 +0400

    Update1.4

--- FrontEndProject/js/index.js
+++ FrontEndProject/js/index.js
@@ -10,6 +10,14 @@ $(document).ready(function () {
     }else{
       $(".navbar").removeClass("sticky")
     }
+    console.log(window.scrollY);
+    if (window.scrollY > 140) {
+      $(".square-card").css("opacity", "1")
+      $(".square-card").css("transform", "translateY(0)")
+    }
+    if (window.scrollY > 700) {
+      $("#cloud-image").css("opacity", "1")
+      $("#cloud-image").css("padding-left", "85px")
+    }
   })
-
 })
